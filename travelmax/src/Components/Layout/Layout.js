bd5804ca57217f00666035f286188eefe3dc5023
E Akita <emmy4nash@gmail.com>
2018-10-24 14:34:49 +0000

    initial commit

---
+++ travelmax/src/Components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Aux from '../HOC/Aux/Aux'
+
+class Layout extends Component{
+    
+    render(){
+        return(<Aux>
+        
+            </Aux>);
+    }
+}
